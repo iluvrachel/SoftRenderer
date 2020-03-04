8b633ce2fbfda11b87308f2e4310ad48944c5765
iluvrachel <854720896@qq.com>
2020-03-04 19:05:23 +0800

    line mesh

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include "tgaimage.h"
+#include "read_obj.h"
 
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red   = TGAColor(255, 0,   0,   255);
@@ -33,14 +34,46 @@ void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
     } 
 } 
 
+void draw_wireframe(std::string obj_path,TGAImage &image){
+    Obj obj;
+    obj.read_obj(obj_path);
+    for(int i=0;i<obj.f_list.size(); i+=3){
+        
+        float x0 = obj.vx_list[obj.f_list[i]];
+        //std::cout<<obj.f_list[i]<<" "<<obj.f_list[i+1]<<std::endl;
+        x0 = (x0+1.)*400/2.;
+        float y0 = obj.vy_list[obj.f_list[i]];
+        y0 = (y0+1.)*400/2.;
+        float x1 = obj.vx_list[obj.f_list[i+1]];
+        x1 = (x1+1.)*400/2.;
+        float y1 = obj.vy_list[obj.f_list[i+1]];
+        y1 = (y1+1.)*400/2.;
+        float x2 = obj.vx_list[obj.f_list[i+2]];
+        x2 = (x2+1.)*400/2.;
+        float y2 = obj.vy_list[obj.f_list[i+2]];
+        y2 = (y2+1.)*400/2.;
+
+        
+        //std::cout<<x0<<" "<<x1<<" "<<x2<<std::endl;
+
+        line(x0,y0,x1,y1,image,white);
+        line(x2,y2,x1,y1,image,white);
+        line(x0,y0,x2,y2,image,white);
+
+    }
+
+}
+
 int main(int argc, char** argv) {
-	TGAImage image(100, 100, TGAImage::RGB);
+	TGAImage image(400, 400, TGAImage::RGB);
 	image.set(52, 41, red);
 
-    line(50,80,10,10,image,white);
+    //line(50,80,10,10,image,white);
+    draw_wireframe("wawa.obj",image);
 
 	image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
     image.write_tga_file("output.tga");
+    system("pause");
 	return 0;
 }
 
---
+++ read_obj.h
@@ -0,0 +1,79 @@
+#ifndef __OBJ_H__
+#define __OBJ_H__
+
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <string>
+#include <iostream>
+
+class Obj {
+    public: 
+        int vn = 0;
+        int fn = 0;
+        std::vector<float> vx_list;
+        std::vector<float> vy_list;
+        std::vector<int> f_list;
+
+        Obj();
+        void read_obj(std::string filename);
+
+};
+
+Obj::Obj(){
+    
+}
+
+
+void Obj::read_obj(std::string file_name){
+    std::ifstream in;
+    in.open(file_name.c_str(), std::ios::in);
+    std::string line;
+    std::string str;
+    float x,y,z;
+    int ix,iy,iz;
+
+    while(!in.eof())
+	{
+        getline(in,line);
+        if(line.size() == 0) continue;
+		if(line[0] == '#') 	 continue;
+        else if(line[0] == 'v'){
+			if(line[1] == 't'){
+                    continue;
+				}
+			else if(line[1] == 'n'){
+                    continue;
+				}
+			else{
+                    std::istringstream IL(line);
+					IL >> str >> x >> y >> z;
+					vx_list.push_back(x);
+					vy_list.push_back(y);
+				}
+			
+		}
+		else if(line[0] == 'f'){
+			std::istringstream IL(line);
+			char c;
+            IL >> str;
+            // while(IL  >> ix >> c>> iy >> c >>iz){
+            //     ix--;
+            //     f_list.push_back(ix);
+            //     std::cout<<ix<<std::endl;
+            // }
+			for(int i = 0;i < 3;i++){	
+				IL  >> ix >> c>> iy >> c >>iz ;
+                f_list.push_back(ix-1);
+                std::cout<<c<<std::endl;
+				
+			}
+             
+        }    
+    }
+    vn = vx_list.size();
+    fn = f_list.size()/3;
+    in.close();
+}
+
+#endif //__OBJ_H__
